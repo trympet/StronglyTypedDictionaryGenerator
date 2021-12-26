b193dd81bd75ad3bd9c1db5dda6c075fd4f77eff
Trym Lund Flogard <trym@flogard.no>
2021-12-26 14:27:40 +0100

    implement interface

--- Demo/PersistedKeyValuePairAccessor.cs
+++ Demo/PersistedKeyValuePairAccessor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +13,36 @@ namespace Demo
 
         public T Get<T>(string name) where T : unmanaged
         {
-            throw new NotImplementedException();
+            return (T)_myPersistedKeyValueStore[name];
+        }
+
+        public string Get(string name)
+        {
+            return (string)_myPersistedKeyValueStore[name];
         }
 
         public bool TryGet<T>(string name, out T value) where T : unmanaged
         {
-            throw new NotImplementedException();
+            var result = _myPersistedKeyValueStore.TryGetValue(name, out object? v);
+            value = v is not null ? (T)v : default;
+            return result;
+        }
+
+        public bool TryGet(string name, [NotNullWhen(true)] out string? value)
+        {
+            var result = _myPersistedKeyValueStore.TryGetValue(name, out object? v);
+            value = (string?)v;
+            return result;
         }
 
         public void Set<T>(string name, T value) where T : unmanaged
         {
-            throw new NotImplementedException();
+            _myPersistedKeyValueStore[name] = value;
+        }
+
+        public void Set(string name, string value)
+        {
+            _myPersistedKeyValueStore[name] = value;
         }
     }
 }
